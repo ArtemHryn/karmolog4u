c54bd107b00cea8dcd90566a10d072587a0daf83
ancorframe <yura.pundiak@gmail.com>
2023-08-31 12:32:50 +0300

    Update SocialLinks.jsx

--- components/SocialLinks/SocialLinks.jsx
+++ components/SocialLinks/SocialLinks.jsx
@@ -9,7 +9,7 @@ import Viber from "@components/Common/SocialIcons/Viber";
 
 const links = [
   { icon: Instagram, to: "https://www.instagram.com/karmolog4u/" },
-  { icon: Telegram, to: "https://t.me/karmolog4u" },
+  { icon: Telegram, to: "https://t.me/karmologforyou" },
   {
     icon: TikTok,
     to: "https://www.tiktok.com/@karmologist?_t=8YIutpJO0g9&_r=1",
@@ -19,7 +19,10 @@ const links = [
     to: "https://api.whatsapp.com/send/?phone=380678696760&text&type=phone_number&app_absent=0",
   },
   { icon: YouTube, to: "https://www.youtube.com/@user-qi5qi3vr9k" },
-  { icon: Viber, to: "viber://chat?number=%2B380678696760" },
+  {
+    icon: Viber,
+    to: "viber://chat/?number=380678696760",
+  },
 ];
 function SocialLinks({ styles }) {
   return (
@@ -28,9 +31,14 @@ function SocialLinks({ styles }) {
       <ul className={styles.list}>
         {links.map(({ icon: Icon, to }, index) => (
           <li key={index}>
-            <Link href={to} className={styles.link}>
+            <a
+              href={to}
+              className={styles.link}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
               <Icon styled={styles.icon_social} />
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
