f845d3807eaff70bb72f6df2046c2dc760ee4b09
ancorframe <yura.pundiak@gmail.com>
2023-09-12 02:07:23 +0300

    Create Hero.jsx

---
+++ components/TherapySessions/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Container from "@components/Common/Container/Container";
+import HeroNav from "@components/Common/HeroNav/HeroNav";
+
+import styles from "./Hero.module.scss";
+import { open_Sans, unbounded } from "@app/layout";
+
+const Hero = ({ linkNames = [], title, img }) => {
+  return (
+    <Container>
+      <HeroNav linkNames={linkNames} />
+      <div className={styles.box}>
+        <div>
+          <h1 className={`${styles.title} ${unbounded.className}`}>{title}</h1>
+          <button className={`${styles.button} ${open_Sans.className}`}>
+            Записатись
+          </button>
+        </div>
+        <Image
+          src={img.img}
+          alt={img.alt}
+          width={1280}
+          height={920}
+          priority={true}
+          className={styles.img}
+        />
+      </div>
+    </Container>
+  );
+};
+
+export default Hero;
