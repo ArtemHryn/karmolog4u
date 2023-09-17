63b6f83a7440f860bf89eaa48a69ac391b3ee413
ancorframe <yura.pundiak@gmail.com>
2023-09-17 09:01:40 +0300

    Create individualTherapyPricing.js

---
+++ helper/individualTherapyPricing.js
@@ -0,0 +1,9 @@
+export const individualPricing = [
+  {
+    title: "4 сесії",
+    addTitle: "з Сергієм Скляренко",
+    price: "1000$",
+  },
+  { title: "6 сесій", addTitle: "з Сергієм Скляренко", price: "1300$" },
+  { title: "8 сесій", addTitle: "з Сергієм Скляренко", price: "1600$" },
+];
