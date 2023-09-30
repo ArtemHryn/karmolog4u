4db6fde9c1d4e60c071b75ee48003965dc94efe9
ancorframe <yura.pundiak@gmail.com>
2023-09-30 12:32:33 +0300

    Update Card.jsx

--- components/Education/WhatIsWaitingForYou/Card.jsx
+++ components/Education/WhatIsWaitingForYou/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ list }) => {
       {list.map((el, index) => (
         <li key={index} className={styles.card_element}>
           {el?.column1 && <p className={styles.column1_text}>{el.column1}</p>}
-          {el?.column1 && <p className={styles.column2_text}>{el.column2}</p>}
+          {el?.column2 && <p className={styles.column2_text}>{el.column2}</p>}
         </li>
       ))}
     </ul>
