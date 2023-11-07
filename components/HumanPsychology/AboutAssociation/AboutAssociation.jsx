8a03167ba8807d9605da7d22a8337f545c5bb55b
Artem <grynbest@gmail.com>
2023-11-08 00:59:22 +0200

    finished the 1st section

---
+++ components/HumanPsychology/AboutAssociation/AboutAssociation.jsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import Link from "next/link";
+import Container from "@components/Common/Container/Container";
+
+import styles from "./AboutAssociation.module.scss";
+
+const AboutAssociation = () => {
+  return (
+    <Container styled={styles.container}>
+      <h1 className="visually-hidden">
+        АСОЦІАЦІЯ “КАРМОТЕРАПІЇ ТА ПСИХОЛОГІЇ”
+      </h1>
+      <div className={styles.card_wrapper}>
+        <picture className={styles.img}>
+          <source
+            srcSet={
+              "/assets/images/humanPsychology/about_association_desc.webp"
+            }
+            media="(min-width: 1280px)"
+          />
+          <Image
+            src={"/assets/images/humanPsychology/about_association.webp"}
+            width={736}
+            height={520}
+            alt="Сергій Скляренко"
+            priority={true}
+            className={styles.img}
+          />
+        </picture>
+        <div className={styles.text_wrapper}>
+          <p className={styles.text}>
+            Кожен день я роблю впевнені кроки до своєї заповітної мрії, яка
+            стала для мене справжнім стилем життя – глобальна трансформація
+            людства. Це та мрія, яка надихає мене до дій, яка мотивує мене і дає
+            наснаги на нові звершення.
+          </p>
+          <p className={styles.text}>
+            Саме тому я вирішив створити організацію та асоціацію, які
+            об’єднають справжніх професіоналів своєї справи. Члени Асоціації
+            згуртовані єдиною ціллю та бажанням: допомогти і підтримати тих,
+            кому це справді потрібно.
+          </p>
+          <p className={styles.text}>
+            Діяльність членів Асоціації спрямована на те, щоб проводити
+            благодійну професійну діяльність задля глобальної трансформації
+            людства в форматі особистої психологічної підтримки, організації
+            колективних семінарів, тренінгів та інших науково-практичних
+            заходів, благодійних зборів
+          </p>
+        </div>
+      </div>
+      <div className={styles.card_wrapper}>
+        <picture className={styles.img}>
+          <source
+            srcSet={
+              "/assets/images/humanPsychology/about_association_2_desc.webp"
+            }
+            media="(min-width: 1280px)"
+          />
+          <Image
+            src={"/assets/images/humanPsychology/about_association_2.webp"}
+            width={736}
+            height={520}
+            alt="Сергій Скляренко"
+            priority={true}
+            className={styles.img}
+          />
+        </picture>
+        <div className={styles.text_wrapper}>
+          <p className={styles.text}>
+            Асоціація &#34;Кармотерапії та психології&#34; - це об&apos;єднання
+            експертів галузі психології та суміжних напрямків.
+          </p>
+          <p className={styles.text}>
+            А це означає, що кожен член Асоціації приймає та підтримує методичні
+            посібники та підходи методу &#34;Кармотерапія&#34; та має на меті
+            головну ціль - бути двигуном глобальної трансформації людства.
+          </p>
+          <p className={styles.text}>
+            * ознайомитись з положенням про асоціацію і ГО можливо{" "}
+            <Link href={"#"}>тут</Link>
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default AboutAssociation;
--- components/Main/PatendedMethods/Slider/Slider.jsx
+++ components/Main/PatendedMethods/Slider/Slider.jsx
@@ -8,8 +8,14 @@ import styles from "../PatentedMethods.module.scss";
 import Title from "@components/Common/Title/Title";
 
 const slides = [
-  { name: "Кармотерапія", link: "/" },
-  { name: "Кармотерапія в подружній парі", link: "/" },
+  {
+    name: "Кармотерапія",
+    link: "https://spinoza.in.ua/2023/09/01/%D0%BA%D0%B0%D1%80%D0%BC%D0%BE%D1%82%D0%B5%D1%80%D0%B0%D0%BF%D1%96%D1%8F-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4-%D1%81%D0%B5%D1%80%D0%B3%D1%96%D1%8F-%D1%81%D0%BA%D0%BB%D1%8F%D1%80%D0%B5%D0%BD%D0%BA%D0%BE/",
+  },
+  {
+    name: "Кармотерапія в подружній парі",
+    link: "https://spinoza.in.ua/2023/10/14/%d0%ba%d0%b0%d1%80%d0%bc%d0%be%d1%82%d0%b5%d1%80%d0%b0%d0%bf%d1%96%d1%8f-%d0%bc%d0%b5%d1%82%d0%be%d0%b4-%d1%81%d0%b5%d1%80%d0%b3%d1%96%d1%8f-%d1%81%d0%ba%d0%bb%d1%8f%d1%80%d0%b5%d0%bd%d0%ba%d0%be-2/",
+  },
 ];
 
 const Slider = () => {
@@ -55,7 +61,11 @@ const Slider = () => {
               <Title variant="p" styled={styles.text}>
                 {el.name}
               </Title>
-              <Link href={el.link} className={styles.button_link}>
+              <Link
+                href={el.link}
+                className={styles.button_link}
+                target="_blank"
+              >
                 Переглянути
               </Link>
             </div>
