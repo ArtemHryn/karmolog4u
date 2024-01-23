3634c8ea8c8da2ba8c67eab77979a020bd9d14f1
Artem <grynbest@gmail.com>
2024-01-23 16:33:24 +0200

    added health, period and current period

--- components/Calculator/PersonalMatrix/PeriodMap/PeriodMap.jsx
+++ components/Calculator/PersonalMatrix/PeriodMap/PeriodMap.jsx
@@ -1,31 +1,54 @@
+import Title from '@components/Common/Title/Title';
+
+import styles from './PeriodMap.module.scss';
+
 const PeriodMap = ({ period }) => {
   if (!period) return;
   return (
-    <div>
-      <ul>
-        <li>
-          <p>Рік</p>
-          <p>Енергія</p>
+    <div className={styles.wrapper}>
+      <Title variant="h3" styled={styles.title}>
+        Карта року
+      </Title>
+      <ul className={styles.name_list}>
+        <li className={styles.name_list_element}>
+          <Title variant="p" styled={styles.column_name}>
+            Рік
+          </Title>
+          <Title variant="p" styled={styles.column_name}>
+            Енергія
+          </Title>
         </li>
-        <li>
-          <p>Рік</p>
-          <p>Енергія</p>
+        <li className={styles.name_list_element}>
+          <Title variant="p" styled={styles.column_name}>
+            Рік
+          </Title>
+          <Title variant="p" styled={styles.column_name}>
+            Енергія
+          </Title>
         </li>
-        <li>
-          <p>Рік</p>
-          <p>Енергія</p>
+        <li className={styles.name_list_element}>
+          <Title variant="p" styled={styles.column_name}>
+            Рік
+          </Title>
+          <Title variant="p" styled={styles.column_name}>
+            Енергія
+          </Title>
         </li>
       </ul>
-      <ul>
-        {period.map((el) => (
-          <li key={el.age}>
-            <p>{el.age}</p>
-            <p>{el.arcane}</p>
+      <ul className={styles.period_table}>
+        {period.map(el => (
+          <li key={el.age} className={styles.period_table_element}>
+            <p className={styles.period_table_text}>{el.age}</p>
+            <p className={styles.period_table_text}>{el.arcane}</p>
           </li>
         ))}
-        <li>
-          <p>80</p>
-          <p>{period[0].arcane}</p>
+        <li className={styles.period_table_element}>
+          <p className={styles.period_table_text}>80</p>
+          <p className={styles.period_table_text}>{period[0].arcane}</p>
+        </li>
+        <li className={styles.period_table_element}>
+          <p className={styles.period_table_text}></p>
+          <p className={styles.period_table_text}></p>
         </li>
       </ul>
     </div>
