b640fd59e77582d17d23d12d1cb69b73bea68535
Artem <grynbest@gmail.com>
2024-04-05 21:26:39 +0300

    changed parents and children svg

--- components/Calculator/ParentsAndChildren/ParentsAndChildrenGraph/ParentsAndChildrenSvg.jsx
+++ components/Calculator/ParentsAndChildren/ParentsAndChildrenGraph/ParentsAndChildrenSvg.jsx
@@ -1,12 +1,12 @@
 const ParentsAndChildrenSvg = ({ styled }) => {
   return (
-    <svg className={styled} viewBox="0 0 618 617" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 619 617" fill="none" xmlns="http://www.w3.org/2000/svg" className={styled}>
       <rect
         x="0.708195"
         y="-0.00109124"
         width="335.12"
         height="335.12"
-        transform="matrix(0.707107 -0.707107 0.709283 0.704924 74.2082 310.172)"
+        transform="matrix(0.707107 -0.707107 0.709283 0.704924 74.7082 310.172)"
         stroke="#454545"
       />
       <rect
@@ -14,7 +14,7 @@ const ParentsAndChildrenSvg = ({ styled }) => {
         y="-0.00109124"
         width="280.149"
         height="280.149"
-        transform="matrix(0.707107 -0.707107 0.709283 0.704924 110.208 311.303)"
+        transform="matrix(0.707107 -0.707107 0.709283 0.704924 110.708 311.303)"
         stroke="#454545"
       />
       <rect
@@ -22,7 +22,7 @@ const ParentsAndChildrenSvg = ({ styled }) => {
         y="0.499998"
         width="269"
         height="269"
-        transform="matrix(1 0 0.00308174 0.999995 176.998 173)"
+        transform="matrix(1 0 0.00308174 0.999995 177.498 173)"
         stroke="#454545"
       />
       <rect
@@ -30,7 +30,7 @@ const ParentsAndChildrenSvg = ({ styled }) => {
         y="0.499998"
         width="197.001"
         height="197.001"
-        transform="matrix(1 0 0.00308174 0.999995 209.998 211)"
+        transform="matrix(1 0 0.00308174 0.999995 210.498 211)"
         stroke="#454545"
       />
       <rect
@@ -38,7 +38,7 @@ const ParentsAndChildrenSvg = ({ styled }) => {
         y="-0.00109124"
         width="197.001"
         height="197.001"
-        transform="matrix(0.707107 -0.707107 0.709283 0.704924 171.208 310.508)"
+        transform="matrix(0.707107 -0.707107 0.709283 0.704924 171.708 310.508)"
         stroke="#454545"
       />
       <rect
@@ -46,78 +46,82 @@ const ParentsAndChildrenSvg = ({ styled }) => {
         y="0.499998"
         width="335.12"
         height="335.12"
-        transform="matrix(1 0 0.00308174 0.999995 141.998 141)"
+        transform="matrix(1 0 0.00308174 0.999995 142.498 141)"
         stroke="#454545"
       />
       <line
         y1="-1"
         x2="279.403"
         y2="-1"
-        transform="matrix(0.708196 -0.706016 0.708196 0.706016 312 506.264)"
+        transform="matrix(0.708196 -0.706016 0.708196 0.706016 312.5 506.264)"
         stroke="#454545"
         strokeWidth="2"
         strokeDasharray="4 4"
       />
-      <line x1="311" y1="589" x2="311" y2="30" stroke="#454545" strokeWidth="2" />
-      <line x1="591" y1="309" x2="32" y2="309" stroke="#454545" strokeWidth="2" />
-      <line x1="113.293" y1="506.566" x2="508.566" y2="111.294" stroke="#454545" strokeWidth="2" />
-      <line x1="113.707" y1="111.293" x2="508.98" y2="506.566" stroke="#454545" strokeWidth="2" />
-      <rect x="112.5" y="110.5" width="397" height="397" stroke="#454545" strokeWidth="3" />
+      <line x1="311.5" y1="589" x2="311.5" y2="30" stroke="#454545" strokeWidth="2" />
+      <line x1="591.5" y1="309" x2="32.5" y2="309" stroke="#454545" strokeWidth="2" />
+      <line x1="113.793" y1="506.566" x2="509.066" y2="111.294" stroke="#454545" strokeWidth="2" />
+      <line x1="114.207" y1="111.293" x2="509.48" y2="506.566" stroke="#454545" strokeWidth="2" />
+      <rect x="113" y="110.5" width="397" height="397" stroke="#454545" strokeWidth="3" />
       <rect
         x="2.12458"
         y="-0.00327373"
         width="397"
         height="397"
-        transform="matrix(0.707107 -0.707107 0.709283 0.704924 28.6246 311.345)"
+        transform="matrix(0.707107 -0.707107 0.709283 0.704924 29.1246 311.345)"
         stroke="#454545"
         strokeWidth="3"
       />
-      <rect x="481.5" y="289.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="481.5" y="289.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="519.5" y="286.5" width="43" height="43" rx="21.5" fill="#101010" />
-      <rect x="519.5" y="286.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
-      <rect x="563.5" y="281.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="563.5" y="281.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="159.5" y="290.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="159.5" y="290.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="0.5" y="283.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="0.5" y="283.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="55.5" y="288.5" width="43" height="43" rx="21.5" fill="#101010" />
-      <rect x="55.5" y="288.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
-      <rect x="99.5" y="291.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="99.5" y="291.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="281.5" y="280.5" width="59" height="59" rx="29.5" fill="#101010" />
-      <rect x="281.5" y="280.5" width="59" height="59" rx="29.5" stroke="#CFB691" />
-      <rect x="292.5" y="480.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="292.5" y="480.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="289.5" y="518.5" width="43" height="43" rx="21.5" fill="#101010" />
-      <rect x="289.5" y="518.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
-      <rect x="283.5" y="562.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="283.5" y="562.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="292.5" y="151.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="292.5" y="151.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="196.5" y="196.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="196.5" y="196.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="196.5" y="388.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="196.5" y="388.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="284" y="0.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="284" y="0.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="289.5" y="55.5" width="43" height="43" rx="21.5" fill="#101010" />
-      <rect x="289.5" y="55.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
-      <rect x="292.5" y="99.5" width="37" height="37" rx="18.5" fill="#101010" />
-      <rect x="292.5" y="99.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
-      <rect x="486.5" y="84.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="486.5" y="84.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="86.5" y="480.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="86.5" y="480.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="483.5" y="482.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="483.5" y="482.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <rect x="85.5" y="85.5" width="54" height="54" rx="27" fill="#101010" />
-      <rect x="85.5" y="85.5" width="54" height="54" rx="27" stroke="#CFB691" />
-      <circle cx="248.5" cy="173" r="5" fill="#CFB691" />
-      <circle cx="177.5" cy="245" r="5" fill="#CFB691" />
-      <circle cx="178.5" cy="376" r="5" fill="#CFB691" />
-      <circle cx="243.5" cy="443" r="5" fill="#CFB691" />
+      <rect x="482" y="289.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="482" y="289.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="520" y="286.5" width="43" height="43" rx="21.5" fill="#101010" />
+      <rect x="520" y="286.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
+      <rect x="564" y="281.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="564" y="281.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="160" y="290.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="160" y="290.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="1" y="283.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="1" y="283.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="56" y="288.5" width="43" height="43" rx="21.5" fill="#101010" />
+      <rect x="56" y="288.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
+      <rect x="100" y="291.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="100" y="291.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="282" y="280.5" width="59" height="59" rx="29.5" fill="#101010" />
+      <rect x="282" y="280.5" width="59" height="59" rx="29.5" stroke="#CFB691" />
+      <rect x="293" y="480.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="293" y="480.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="290" y="518.5" width="43" height="43" rx="21.5" fill="#101010" />
+      <rect x="290" y="518.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
+      <rect x="284" y="562.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="284" y="562.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="293" y="151.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="293" y="151.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="197" y="196.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="197" y="196.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="197" y="388.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="197" y="388.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="284.5" y="0.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="284.5" y="0.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="290" y="55.5" width="43" height="43" rx="21.5" fill="#101010" />
+      <rect x="290" y="55.5" width="43" height="43" rx="21.5" stroke="#CFB691" />
+      <rect x="293" y="99.5" width="37" height="37" rx="18.5" fill="#101010" />
+      <rect x="293" y="99.5" width="37" height="37" rx="18.5" stroke="#CFB691" />
+      <rect x="487" y="84.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="487" y="84.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="87" y="480.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="87" y="480.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="484" y="482.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="484" y="482.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="86" y="85.5" width="54" height="54" rx="27" fill="#101010" />
+      <rect x="86" y="85.5" width="54" height="54" rx="27" stroke="#CFB691" />
+      <rect x="235" y="158.5" width="29" height="29" rx="14.5" fill="#101010" />
+      <rect x="235" y="158.5" width="29" height="29" rx="14.5" stroke="#CFB691" />
+      <rect x="164" y="228.5" width="29" height="29" rx="14.5" fill="#101010" />
+      <rect x="164" y="228.5" width="29" height="29" rx="14.5" stroke="#CFB691" />
+      <rect x="164" y="362.5" width="29" height="29" rx="14.5" fill="#101010" />
+      <rect x="164" y="362.5" width="29" height="29" rx="14.5" stroke="#CFB691" />
+      <rect x="230" y="428.5" width="29" height="29" rx="14.5" fill="#101010" />
+      <rect x="230" y="428.5" width="29" height="29" rx="14.5" stroke="#CFB691" />
     </svg>
   );
 };
