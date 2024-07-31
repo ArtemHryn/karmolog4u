724f5092faccbcfed4f24f41ae3ffc3476a63c0f
Artem <grynbest@gmail.com>
2024-07-31 14:15:22 +0300

    demon calc translation

--- components/Calculator/DemonMatrix/DemonMatrixGraph/DemonMatrixGraph.jsx
+++ components/Calculator/DemonMatrix/DemonMatrixGraph/DemonMatrixGraph.jsx
@@ -1,20 +1,22 @@
+import { useTranslations } from 'next-intl';
 import Title from '@components/Common/Title/Title';
 import DemonMatrixSvg from './DemonMatrixSvg';
 import Top from '@components/Calculator/PersonalMatrix/MatrixGraph/Top';
 import Left from '@components/Calculator/PersonalMatrix/MatrixGraph/Left';
 import Right from '@components/Calculator/PersonalMatrix/MatrixGraph/Right';
 import Bottom from '@components/Calculator/PersonalMatrix/MatrixGraph/Bottom';
-
-import styles from './DemonMatrixGraph.module.scss';
 import Center from '@components/Calculator/PersonalMatrix/MatrixGraph/Center';
 
+import styles from './DemonMatrixGraph.module.scss';
+
 const DemonMatrixGraph = ({ matrix, code, date }) => {
+  const t = useTranslations('Calculator.demon');
   if (!matrix) return null;
   return (
     <div>
       <div className={styles.date_wrapper}>
         <Title variant="p" styled={styles.date_title}>
-          Сумісність з {code} кодом
+          {t('graph_title', { code: code })}
         </Title>
         <Title variant="p" styled={styles.date}>
           {date}
@@ -26,7 +28,7 @@ const DemonMatrixGraph = ({ matrix, code, date }) => {
         <Left matrix={matrix} hideAdditionalKeys />
         <Right matrix={matrix} hideAdditionalKeys />
         <Bottom matrix={matrix} hideAdditionalKeys hideInnerAdditionalKeys />
-        <Center matrix={matrix} hideCenter2/>
+        <Center matrix={matrix} hideCenter2 />
       </div>
     </div>
   );
