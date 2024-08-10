12cd5f27fc2ce468ac21b41a392b9c369cc4ef48
Artem <grynbest@gmail.com>
2024-08-10 22:00:34 +0300

    public speeches page translation

--- components/Services/Offline-meetings/SpeechesExamples/SpeechesExamples.jsx
+++ components/Services/Offline-meetings/SpeechesExamples/SpeechesExamples.jsx
@@ -1,48 +1,60 @@
-import Container from "@components/Common/Container/Container";
-import Title from "@components/Common/Title/Title";
-import Video from "../HowIsGoingNailing/Video";
-import SeeMoreOnMyYouTube from "@components/Common/SeeMoreOnMyYouTube/SeeMoreOnMyYouTube";
+import Container from '@components/Common/Container/Container';
+import Title from '@components/Common/Title/Title';
+import Video from '../HowIsGoingNailing/Video';
+import SeeMoreOnMyYouTube from '@components/Common/SeeMoreOnMyYouTube/SeeMoreOnMyYouTube';
 
-import styles from "./SpeechesExamples.module.scss";
+import styles from './SpeechesExamples.module.scss';
+import { useLocale, useTranslations } from 'next-intl';
 
 const examples = [
   {
-    id: "1",
-    videoId: "BhmkDrlU8Cs?si=6hjwrji_nJJML6xD",
-    title: "Жінка очима чоловіка",
+    videoId: 'BhmkDrlU8Cs?si=6hjwrji_nJJML6xD',
+    title: { uk: 'Жінка очима чоловіка', ru: 'Женщина глазами мужчины' },
   },
   {
-    id: "2",
-    videoId: "EBqm4_GGeV8?si=3h6VUk-1v-xZcF6V",
-    title: "Карма та доля людини . Що можна дізнатися за допомогою матриці ?",
+    videoId: 'z4mgRLcsv48?si=u97Dki9z1u-LqQz3',
+    title: {
+      uk: 'Сила роду , карма батьків, як змінити долю',
+      ru: 'Сила рода, карма родителей, как изменить судьбу',
+    },
   },
   {
-    id: "3",
-    videoId: "z4mgRLcsv48?si=u97Dki9z1u-LqQz3",
-    title: "Сила роду , карма батьків, як змінити долю",
+    videoId: 'EBqm4_GGeV8?si=3h6VUk-1v-xZcF6V',
+    title: {
+      uk: 'Карма та доля людини . Що можна дізнатися за допомогою матриці?',
+      ru: 'Карма и судьба человека. Что можно узнать с помощью матрицы?',
+    },
+  },
+
+  {
+    videoId: 'K-Rhc_dFkzY?si=ImYFol8_L8JVYHqS',
+    title: {
+      uk: 'Любов до себе та взаємозвʼязок з навколишнім світом',
+      ru: 'Любовь к себе и взаимосвязь с окружающим миром и окружающим миром',
+    },
   },
   {
-    id: "4",
-    videoId: "K-Rhc_dFkzY?si=ImYFol8_L8JVYHqS",
-    title: "Любов до себе та взаємозвʼязок з навколишнім світом",
-  },
-  {
-    id: "5",
-    videoId: "QQUWJRoNQa4?si=UdqLY2ruOCNwOBQQ",
-    title: "Як визначити справжні бажання?",
+    videoId: 'QQUWJRoNQa4?si=UdqLY2ruOCNwOBQQ',
+    title: { uk: 'Як визначити справжні бажання?', ru: 'Как определить истинные желания?' },
   },
 ];
 
 const SpeechesExamples = () => {
+  const t = useTranslations('Services.offline_meetings.public_speeches.speeches_examples');
+  const locale = useLocale();
   return (
     <Container styled={styles.container}>
-      <Title styled={styles.title_mob}>Приклади публіч - них виступів:</Title>
-      <Title styled={styles.title}>Приклади публічних виступів:</Title>
+      <Title variant="h2" styled={styles.title_mob}>
+        {t('small_devices_title')}
+      </Title>
+      <Title variant="h2" styled={styles.title}>
+        {t('title')}
+      </Title>
       <ul className={styles.video_list}>
-        {examples.map((example) => (
-          <li key={example.id}>
+        {examples.map((example, index) => (
+          <li key={index}>
             <Title styled={styles.video_title} variant="p">
-              {example.title}
+              {example.title[locale]}
             </Title>
             <Video id={example.videoId} />
           </li>
