53b0d6edf9ec2cb788ae0e48dc24a41785e7648f
Artem <grynbest@gmail.com>
2024-08-24 17:03:12 +0300

    added translation of advanced course page

--- components/Education/EduPricing/EduPricing.jsx
+++ components/Education/EduPricing/EduPricing.jsx
@@ -3,11 +3,19 @@ import styles from './EduPricing.module.scss';
 import Title from '@components/Common/Title/Title';
 import Link from 'next/link';
 import { open_Sans } from '@app/[locale]/layout';
+import { useLocale, useTranslations } from 'next-intl';
 
 function EduPricing({ card, addInfo = [], desc, link }) {
+  const locale = useLocale();
+  const t = useTranslations('Education.advanced_course.edu_pricing');
+
+  const renderAddInfo = Array.isArray(addInfo) ? addInfo : addInfo[locale];
+
   return (
     <Container>
-      <Title styled={styles.title}>Вартість навчання</Title>
+      <Title variant="h2" styled={styles.title}>
+        {t('title')}
+      </Title>
       <p className={styles.desc}>
         {desc.first}
         <br />
@@ -17,25 +25,25 @@ function EduPricing({ card, addInfo = [], desc, link }) {
         <div className={styles.card}>
           <div className={styles.wrap}>
             <Title variant="h4" styled={styles.card_title}>
-              {card.title}
+              {typeof card.title === 'string' ? card.title : card.title[locale]}
             </Title>
             <div className={styles.card_price_wrap}>
               <Title variant="h3" styled={styles.card_price}>
                 {card.price}
               </Title>
               <Title variant="h5" styled={styles.card_info}>
-                (повна вартість)
+                ({t('full_payment')})
               </Title>
             </div>
           </div>
           <Link href={link} className={`${styles.button} ${open_Sans.className}`}>
-            Записатися
+            {t('button')}
           </Link>
         </div>
       </div>
       <section>
         <ul className={styles.info}>
-          {addInfo.map((item, index) => (
+          {renderAddInfo.map((item, index) => (
             <li key={index}>
               <div className={styles.info_card}>
                 <p className={styles.info_text}>{item}</p>
